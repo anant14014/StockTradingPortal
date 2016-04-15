6229c10e6a1910856f2b1ccb679a4703b17922f4
anant14014 <anant14014@iiitd.ac.in>
2016-04-15 16:29:52 +0530

    Added a few new views and the stocks filter controller

---
+++ StockTradingPortal/src/stockportal/controller/StocksFilterController.java
@@ -0,0 +1,26 @@
+package stockportal.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class StocksFilterController {
+	
+	@FXML
+	private TextField nameField;
+	@FXML
+	private TextField valueToField;
+	@FXML
+	private TextField valueFromField;
+	
+	@FXML
+	private Button submitButton;
+	
+	@FXML
+    void initialize() {
+    }
+	
+	public void handleSubmit() {
+		
+	}
+	
+}
